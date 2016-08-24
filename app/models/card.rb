289a203487b3de4faa868e1af85c04039a5ed247
gvoz <gvozdmeister@gmail.com>
2016-08-24 23:13:38 +0300

    fix comments

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,11 +1,9 @@
 class Card < ApplicationRecord
-  validates :original_text, presence: { message: 'должен быть указан оригинальный текст' }
-  validates :translated_text, presence: { message: 'должен быть указан переведённый текст' }
-  validates :review_date, presence: { message: 'должна быть указана дата пересмотра карточки' }
+  validates :original_text, :translated_text, :review_date, presence: true
   validate :original_and_translated_text_not_equal
 
   after_initialize do
-    self.review_date = Date.today.next_day(3) if review_date.nil?
+    self.review_date = 3.days.from_now if review_date.nil?
   end
 
   def original_and_translated_text_not_equal
