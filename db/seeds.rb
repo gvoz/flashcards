e919096824660969feddd1e4c3355e8f367dbb67
gvoz <gvozdmeister@gmail.com>
2016-08-25 20:06:47 +0300

    comments houndci

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ def parsing_table(table)
   table.xpath("//tr[position()>1]").each do |row|
     original = row.xpath("td[2]").text
     translated = row.xpath("td[3]").text
-    Card.create( original_text: original, translated_text: translated, review_date: 3.days.from_now )
+    Card.create(original_text: original, translated_text: translated, review_date: 3.days.from_now)
   end
 end
 
@@ -21,5 +21,5 @@ page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary
 
 parsing_table(page)
 
-links = page.xpath("//a").select{|link| link.text.include?("Most common German words") }
-links.each{|link| parsing_table(Nokogiri::HTML(open("http://www.languagedaily.com" + link['href']))) }
+links = page.xpath("//a").select { |link| link.text.include?("Most common German words") }
+links.each { |link| parsing_table(Nokogiri::HTML(open("http://www.languagedaily.com" + link['href']))) }
