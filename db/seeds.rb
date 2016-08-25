2cbef93cebc1cccf2117b3d7d896b7aa20e103c2
gvoz <gvozdmeister@gmail.com>
2016-08-25 19:59:02 +0300

    parsing all translation words

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,17 @@
 require "nokogiri"
 require "open-uri"
 
-@page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
-
-@page.xpath("//tr").each do |row|
-  original = row.xpath("td[2]").text
-  translated = row.xpath("td[3]").text
-  Card.create( original_text: original, translated_text: translated, review_date: 3.days.from_now )
+def parsing_table(table)
+  table.xpath("//tr[position()>1]").each do |row|
+    original = row.xpath("td[2]").text
+    translated = row.xpath("td[3]").text
+    Card.create( original_text: original, translated_text: translated, review_date: 3.days.from_now )
+  end
 end
+
+page = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/common-german-words"))
+
+parsing_table(page)
+
+links = page.xpath("//a").select{|link| link.text.include?("Most common German words") }
+links.each{|link| parsing_table(Nokogiri::HTML(open("http://www.languagedaily.com" + link['href']))) }
