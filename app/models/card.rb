558bccfd3b3bd0c60219a69b0ac42e4c2d0d92b7
gvoz <gvozdmeister@gmail.com>
2016-08-28 20:08:10 +0300

    testing model

--- app/models/card.rb
+++ app/models/card.rb
@@ -9,19 +9,17 @@ class Card < ApplicationRecord
   def original_and_translated_text_not_equal
     unless original_text.blank? || translated_text.blank?
       errors.add(:original_text, 'не должен совпадать с переведённым') if
-        original_text.mb_chars.downcase == translated_text.mb_chars.downcase
+        original_text.strip.mb_chars.downcase == translated_text.strip.mb_chars.downcase
     end
   end
 
-  def self.check_translate?(id, user_text)
-    @card = Card.find(id)
-    if user_text.mb_chars.downcase == @card.original_text.mb_chars.downcase
-      @card.review_date = 3.days.from_now
-      @card.save
-      return true
-    else
-      return false
-    end
+  def check_translation?(user_text)
+    user_text.strip.mb_chars.downcase == original_text.mb_chars.downcase
+  end
+
+  def change_review_date
+    self.review_date = 3.days.from_now
+    self.save
   end
 
   scope :reviewed, -> { where('review_date <= ?', Time.now) }
