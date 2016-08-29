31d585b5de5ed63526af23826c0bd803bc6e569f
gvoz <gvozdmeister@gmail.com>
2016-08-29 17:06:55 +0300

    configure travis ci

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -1,11 +1,19 @@
 require "rails_helper"
 
 feature "Card managment", :type => :feature do
+  before(:each) do
+    card = create(:card)
+  end
+
+  scenario "No card to translate" do
+    visit root_path
+    expect(page).to have_content "Нет актуальных карточек для перевода"
+  end
+
   scenario "Check user translation" do
     visit root_path
-    fill_in :user_text, :with => "du"
+    fill_in :user_text, :with => "und"
     click_button "Проверить перевод"
-
-    expect(page).to have_content "Неправильный перевод"
+    expect(page).to have_content "Правильный перевод"
   end
 end
