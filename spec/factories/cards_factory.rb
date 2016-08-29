d770baa8069409eb0e7d6ec196ce0321657735b6
gvoz <gvozdmeister@gmail.com>
2016-08-29 17:23:37 +0300

    use feature specs

--- spec/factories/cards_factory.rb
+++ spec/factories/cards_factory.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :card do
     original_text "hause"
     translated_text "house"
-    review_date 15.days.from_now
+    review_date -15.days.from_now
   end
 end
--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -1,19 +1,25 @@
 require "rails_helper"
 
 feature "Card managment", :type => :feature do
-  before(:each) do
-    card = create(:card)
-  end
-
   scenario "No card to translate" do
     visit root_path
     expect(page).to have_content "Нет актуальных карточек для перевода"
   end
 
   scenario "Check user translation" do
+    card = create(:card)
+    card2 = create(:card)
     visit root_path
-    fill_in :user_text, :with => "und"
+    fill_in :user_text, :with => "hause"
     click_button "Проверить перевод"
     expect(page).to have_content "Правильный перевод"
+  end
+
+  scenario "Last card" do
+    card = create(:card)
+    visit root_path
+    fill_in :user_text, :with => "hause"
+    click_button "Проверить перевод"
+    expect(page).to have_content "Нет актуальных карточек для перевода"
   end
 end
