8da121cf111b1db57c6ae1f0565aa92bd5349a8e
gvoz <gvozdmeister@gmail.com>
2016-08-29 19:17:23 +0300

    add button with status travis

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -1,24 +1,24 @@
 require "rails_helper"
 
-feature "Card managment", :type => :feature do
+feature "Card managment", type: :feature do
   scenario "No card to translate" do
     visit root_path
     expect(page).to have_content "Нет актуальных карточек для перевода"
   end
 
   scenario "Check user translation" do
-    card = create(:card)
-    card2 = create(:card)
+    create(:card)
+    create(:card)
     visit root_path
-    fill_in :user_text, :with => "hause"
+    fill_in :user_text, with: "hause"
     click_button "Проверить перевод"
     expect(page).to have_content "Правильный перевод"
   end
 
   scenario "Last card" do
-    card = create(:card)
+    create(:card)
     visit root_path
-    fill_in :user_text, :with => "hause"
+    fill_in :user_text, with: "hause"
     click_button "Проверить перевод"
     expect(page).to have_content "Нет актуальных карточек для перевода"
   end
